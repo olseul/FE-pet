5992ca77a089a30233bdfdc4f2efeef4d7e55842
손지은 <wiseag@naver.com>
2024-01-18 19:42:07 +0900

    🧹 chore : 초기 세팅

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "반려일기",
+  description: "반려동물의 일상을 기록하고 공유하세요",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import * as styles from '@/app/page.css';
+const Home = () => {
+  return <div className={styles.container}>home</div>;
+};
+
+export default Home;

77b8e1ab0d0d436ad77d12dfe96f06ff38401d47
손지은 <wiseag@naver.com>
2024-01-22 11:41:19 +0900

    feat : QueryClientProvider 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,17 @@
+import QueryProvider from "@/components/QueryProvider";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-// import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "반려일기",
   description: "반려동물의 일상을 기록하고 공유하세요",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
